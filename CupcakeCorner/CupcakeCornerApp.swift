5e63f1d1fefe7adda780938abfdbc09b59914977
Brandon Glenn <Brandon.Glenn@icloud.com>
2021-12-16 12:26:50 -0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Brandon Glenn on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
